b22fc8a3607f5290f8c3325d64d5900e41ae8c72
michael.schiff <michael.schiff@tubemogul.com>
2014-01-30 11:18:09 -0800

    removed all the merge conflicts

--- src/main/scala/harness.scala
+++ src/main/scala/harness.scala
@@ -1,10 +1,6 @@
 import scala.io.Source
-<<<<<<< HEAD
-import BIDMat.FMat
 import BIDMat.MatFunctions.{row, col}
-=======
 import BIDMat.DMat
->>>>>>> SGD seems to diverge
 import scala.collection.mutable.ListBuffer
 import Classifiers._
 
@@ -18,38 +14,17 @@ package Main {
       var i: Int = 0
       for( line <- Source.fromFile("train_cleaned.csv").getLines() ) {
         if (i > 0) {
-<<<<<<< HEAD
           val r = line.split(",").map(x => x.toFloat)
           val (example, label) = r.splitAt(r.length-1)
           X_train += row(example)
           Y_train += col(label)
-=======
-          val splits: ListBuffer[Double] = new ListBuffer()
-          for (f <- line.split(",").map(x => x.toDouble)) {
-            splits.append(f)
-          }
-          val xarr: Array[Double] = new Array[Double](778)
-          val yarr: Array[Double] = new Array[Double](1)
-
-          splits.slice(1, splits.size-1).copyToArray(xarr)
-          splits.slice(splits.size-1, splits.size).copyToArray(yarr)
-          val xmat: DMat = new DMat(1, splits.size-2, xarr)
-          val ymat: DMat = new DMat(1, 1, yarr)
-          X_train.append(xmat)
-          Y_train.append(ymat)
->>>>>>> SGD seems to diverge
         }
         i += 1
         if (1%10000 == 0) { println(i + " rows consumed") }
       }
       println("" + X_train.size + " rows of data")
-      
-<<<<<<< HEAD
-      val classifier = new LRClassifier(X_train, Y_train, 0.00000001, 0.0001)
-=======
       val classifier = new LRClassifier(X_train, Y_train, 0.0000000000000000000000000000000000000001d, 0.0001)
       println("The labels: " + Y_train)
->>>>>>> SGD seems to diverge
       classifier.train()
       println("Weights: " + classifier.weights)
 
