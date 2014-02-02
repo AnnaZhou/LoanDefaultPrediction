b6de89842df0c048f17ac1c931a97517e8ea1924
Will Skinner <meaningoflifeis42@gmail.com>
2014-02-01 22:05:53 -0800

    pickling the model

--- python/rf_classify.py
+++ python/rf_classify.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
 from sklearn import datasets
 from sklearn.grid_search import GridSearchCV
+from sklearn.externals import joblib
 
 import numpy as np
 
@@ -18,12 +19,14 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(data[:, 1:-
 
 model = RandomForestClassifier(n_jobs=-1, max_depth=20, n_estimators=900, verbose=2)
 model.fit(X_train, y_train)
+joblib.dump(model, 'random_forest_cl.pkl')
+
 print 'Finished fitting the model'
 
 result = model.score(X_test, y_test)
 print 'Results:'
 print result
 
-report = classification_report(y_test, model.predict(y_test))
+report = classification_report(y_test, model.predict(X_test))
 print 'Report:'
 print report
