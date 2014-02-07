6293be4fc2200b248f10bee7baeeb88ee178e82f
Will Skinner <meaningoflifeis42@gmail.com>
2014-02-06 21:46:10 -0800

    move to reflect classifier type

--- python/lr_rf_predictor.py
+++ python/lr_rf_predictor.py
@@ -19,10 +19,11 @@ if len(sys.argv) > 2:
     y_test = None
 else:
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(X_train, y_train, test_size=0.1, random_state=0)
+
     
 
-clf = Pipeline([('Scale', StandardScaler()), ('SVM', LinearSVC(C=0.01, penalty="l1", dual=False, verbose=2))])
-reg = Pipeline([('Scale', StandardScaler()), ('Random Forest', RandomForestRegressor(n_jobs=8, max_depth=6, n_estimators=900, verbose=2))])
+clf = Pipeline([('Scale', StandardScaler()), ('Classifier', LogisticRegression(C=0.01, penalty="l1", dual=False, verbose=2, class_weight='auto'))])
+reg = Pipeline([('Scale', StandardScaler()), ('Regressor', RandomForestRegressor(n_jobs=8, max_depth=6, n_estimators=900, verbose=2))])
 
 model = UpsamplingPredictor(clf, reg)
 model.fit(X_train, X_test, y_train, y_test, bin_thresh=1)
